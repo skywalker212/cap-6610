3002ed812e7a192f0fa417babf0d0bd3f99e1e26
Akash Gajjar <akashgajjar8@gmail.com>
2023-02-01 14:46:57 -0500

    add ridge regression

--- project-1/data.py
+++ project-1/data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.matlib
 import math
 
 def generate_datapoint(K, Theta):
@@ -10,17 +11,19 @@ def generate_datapoint(K, Theta):
 def generate_data(N, K):
     Theta = np.random.default_rng().standard_normal((1, K))
     Samples = np.empty((0, K+1))
-    for i in range(N):
+    for _ in range(N):
         Sample = generate_datapoint(K, Theta)
         Samples = np.append(Samples, Sample, axis=0)
     return Theta, Samples
 
-def calculate_l2_norm(X, Theta, Y):
-    print(X.shape, Theta.shape)
-    Diff = np.matmul(X, Theta.transpose()) - Y
-    SquaredDiff = np.square(Diff)
+def calculate_squared_error(Array):
+    SquaredDiff = np.square(Array)
     Loss = np.sum(SquaredDiff)
     return Loss
+
+def calculate_loss(X, Theta, Y):
+    return calculate_squared_error(np.matmul(X, Theta.transpose()) - Y)
+
 
 K = 10
 N = 3
@@ -28,5 +31,12 @@ Theta, Data = generate_data(N, K)
 X, Y = Data[:, :-1], Data[:, -1].reshape(N, 1)
 
 ThetaStar = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.transpose(), X)), X.transpose()), Y).reshape(1, K)
-Loss = calculate_l2_norm(X, ThetaStar, Y)
-print(Loss)
+Loss = calculate_loss(X, ThetaStar, Y)
+Distance = calculate_squared_error(Theta - ThetaStar)
+print(Loss, Distance)
+
+Lambda = 0.5
+RidgeTheta = np.matmul(np.matmul(np.linalg.inv(np.matmul(X.transpose(), X) + Lambda ** 2 * np.matlib.identity(K)), X.transpose()), Y).reshape(1, K)
+RidgeLoss = calculate_loss(X, RidgeTheta, Y)
+RidgeDistance = calculate_squared_error(Theta - RidgeTheta)
+print(RidgeLoss, RidgeDistance)
